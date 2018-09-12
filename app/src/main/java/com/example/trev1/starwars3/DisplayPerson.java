f259cf8157d7a3b47aefa3889f9deb00d0d8d2c6
trev125 <trev125@gmail.com>
2018-09-11 21:04:17 -0600

    Added Comments!! Now it should be easy to understand what I did on all of the pages (hopefully)

--- app/src/main/java/com/example/trev1/starwars3/DisplayPerson.java
+++ app/src/main/java/com/example/trev1/starwars3/DisplayPerson.java
@@ -1,14 +1,28 @@
 package com.example.trev1.starwars3;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class DisplayPerson extends AppCompatActivity {
+    /*
+    *  This is where I am building the DisplayPerson page, which will be used for displaying all of
+    *  the data I got from the API. I was originally making separate pages for each person (stupid,
+    *  I know) then I realized I could use one page and just change the information dynamically. The
+    *  only thing I had an issue with changing dynamically was the Title of the activity. For whatever
+    *  reason whenever I tried to set that it would only set for the previous person I visited.
+    *      Ex: I visit Luke Skywalker page and the title is set as null. I then go back and visit the
+    *      Yoda page, the title will be "Luke Skywalker". Then I go back and click on Leia, the title
+    *      will be "Yoda".
+    *  So I tried fixing that for a bit, but after a few hours of googling and asking others, I settled
+    *  on setting it as null and just loading my own title above the image. Not ideal :(
+    *
+    *  Back to the page, I create all of the TextViews, RadioButtons and ImageViews here so I can
+    *  access them in the Async tasks. I then set them equal to all of the IDs that I have on the
+    *  activity page so that I can actually load in the data. That's all that's on this page.
+    */
     public static TextView tvName;
     public static TextView tvHeight;
     public static TextView tvWeight;
--- app/src/main/java/com/example/trev1/starwars3/fetchData.java
+++ app/src/main/java/com/example/trev1/starwars3/fetchData.java
@@ -1,13 +1,8 @@
 package com.example.trev1.starwars3;
 
-import android.app.Activity;
 import android.os.AsyncTask;
-import android.renderscript.ScriptGroup;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,8 +13,10 @@ import java.net.URL;
 import java.text.DecimalFormat;
 
 public class fetchData extends AsyncTask<Integer,Void,Void> {
+    /* I set these variables here so I can access them in my onPostExecute function so I don't have
+    *  to worry about passing the variables down there.
+    */
     String data = "";
-    public String dataParsed = "";
     public String nameParsed = "";
     String heightParsed = "";
     String weightParsed = "";
@@ -33,10 +30,47 @@ public class fetchData extends AsyncTask<Integer,Void,Void> {
 
     @Override
     protected Void doInBackground(Integer... integers) {
+        /*
+        *  A lot happens in this function, so I'll try to break it all down so it makes sense. I'll
+        *  also try to label all of the sections below with what they are doing.
+        *       1. I need to find out what person I am parsing the data for, so I passed in a number
+        *          when I called this Async function, and I take that and concatenate it directly into
+        *          the URL.
+        *
+        *       2. Then I get all the data from the actual JSON object. I was lucky with this API
+        *          because none of the data I wanted was in an Array. (The HomePlanet was in it's
+        *          own JSON object, but we wll get to that later.)
+        *
+        *       3. Next, I loaded it into a JSON object, and started to set the data to the local
+        *          variables that I created earlier. I accessed them using the Keys that are in the
+        *          JSON objects.
+        *
+        *          3a. Things got a little funky sometimes. I ended up having to convert some of the
+        *              data that I got to some more readable stuff. I adapted a lot of the conversion
+        *              functions (convertHeight and convertWeight) from different forums online.
+        *              Basically, I just took CM and KG and converted them to Feet and Inches or Pounds.
+        *
+        *          3b. Similarly, there were some strings that I got back from the JSON object that
+        *              were not capitalized and that bothered me, so I fixed that.
+        *
+        *          3c. This was a doozie. Kinda. The HomePlanet information was actually a URL to
+        *              another API call. I ended up parsing that in its own function to just get the
+        *              name of it. It wasn't too bad. I probably could have changed up the original
+        *              parser I used in step 3, but I would have to change a lot of the ways I was
+        *              handling data, so I decided to write another function.
+        *
+        *       4. Onto the onPostExecute function! Okay so here I create an array of the images I
+        *          want to show on the specific pages of the people. Then I access the different
+        *          elements of the DisplayPerson activity and set the information of the person. The
+        *          only thing that I had to be careful with here was the Gender and the Top Image.
+        *          Because those can be different per person, I did the if/else statements.
+        *
+        *  That's it! That's all of the project, hopefully explained well enough.
+        */
         try {
-            int number = integers[0];
-            URL url = new URL("https://swapi.co/api/people/" + number + "/?format=json");//https://api.myjson.com/bins/ap3ok
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            int number = integers[0];//Step 1
+            URL url = new URL("https://swapi.co/api/people/" + number + "/?format=json");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();//Step 2
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             String line = "";
@@ -45,23 +79,23 @@ public class fetchData extends AsyncTask<Integer,Void,Void> {
                 data = data + line;
             }
 
-            JSONObject JO = new JSONObject(data);
+            JSONObject JO = new JSONObject(data);//Step 3
             nameParsed = "" + JO.get("name");
             heightParsed = "" + JO.get("height");
             weightParsed = "" + JO.get("mass");
             heightCM = Integer.parseInt(heightParsed);
-            heightParsed = "" + convertHeight(heightCM);
+            heightParsed = "" + convertHeight(heightCM);//Step 3a
             weightKG = Integer.parseInt(weightParsed);
-            weightParsed = "" + convertWeight(weightKG);
+            weightParsed = "" + convertWeight(weightKG);//Step3a
             genderParsed = "" + JO.get("gender");
-            genderParsed = genderParsed.substring(0, 1).toUpperCase() + genderParsed.substring(1).toLowerCase();
-            //dataParsed =  "Name: " + nameParsed + "\n" + "Height: " + heightParsed + "\n" + "Weight: " + weightParsed+ "\n" + "Gender: " + genderParsed;
+            genderParsed = genderParsed.substring(0, 1).toUpperCase() + genderParsed.substring(1).toLowerCase();//Step 3b
             homeWorldParsed = "" + JO.get("homeworld");
-            homeWorldParsed = homeworldParse(homeWorldParsed);
+            homeWorldParsed = homeworldParse(homeWorldParsed);//Step 3c
+            homeWorldParsed = homeWorldParsed.substring(0, 1).toUpperCase() + homeWorldParsed.substring(1).toLowerCase();//Step 3b
             hairColorParsed = "" + JO.get("hair_color");
-            hairColorParsed = hairColorParsed.substring(0, 1).toUpperCase() + hairColorParsed.substring(1).toLowerCase();
+            hairColorParsed = hairColorParsed.substring(0, 1).toUpperCase() + hairColorParsed.substring(1).toLowerCase();//Step 3b
             eyeColorParsed = "" + JO.get("eye_color");
-            eyeColorParsed = eyeColorParsed.substring(0, 1).toUpperCase() + eyeColorParsed.substring(1).toLowerCase();
+            eyeColorParsed = eyeColorParsed.substring(0, 1).toUpperCase() + eyeColorParsed.substring(1).toLowerCase();//Step 3b
             birthYearParsed = "" + JO.get("birth_year");
 
         }catch (MalformedURLException e){
@@ -75,25 +109,36 @@ public class fetchData extends AsyncTask<Integer,Void,Void> {
     }
 
     protected String convertHeight(int height){
+        //Like I said above, this is just to change the height from CM to feet and inches
         DecimalFormat df = new DecimalFormat("#.#");
-        String heightString = "";
+        String heightString;
+        double cmConst = 30.48;
+        double inConst = 2.54;
+        int ftConst = 12;
+
         int cm = height;
-        double feet = cm/30.48;
-        double inches = (cm/2.54) - ((int)feet * 12);
+        double feet = cm/cmConst;
+        double inches = (cm/inConst) - ((int)feet * ftConst);
         String formatInches = df.format(inches);
         heightString = "" + (int)feet + " feet and " + formatInches + " inches";
         return heightString;
     }
 
     protected String convertWeight(int weight){
+        //Like I said above, this is just to convert from KG to Pounds.
         DecimalFormat df = new DecimalFormat("#.#");
         double conversion = 2.20462;
+
         double weightLB = weight * conversion;
         String weightFormatted = df.format(weightLB) + " pounds";
         return weightFormatted;
     }
 
     protected String homeworldParse (String homeworld) {
+        /*
+        * This is the second parse function to get the Home World name for the person. It is
+        * essentially the same function that I use earlier to get the personal information.
+        */
         String homeworldURL = homeworld;
         String homeworldName = "";
         try {
@@ -121,61 +166,64 @@ public class fetchData extends AsyncTask<Integer,Void,Void> {
 
     @Override
     protected void onPostExecute(Void aVoid) {
+        /*
+        *  Like I said above, this is how I communicate with the data I have being built on a different
+        *  thread that is constructing the actual UI. I take all the data I have gotten from the API
+        *  and I am setting it to the different TextViews I have set up to receive them. Gender is
+        *  handled a bit differently because its a radio button. Same with images because I wanted
+        *  them to be different depending on who you were viewing. The Image is generally the Home
+        *  Planet of the person. The exception is Yoda because his Home Planet is "Unknown" so I did
+        *  a picture of Dagobah.
+        */
         super.onPostExecute(aVoid);
-        String name = this.nameParsed;
-        String height = this.heightParsed;
-        String weight = this.weightParsed;
-        String homeWorld = this.homeWorldParsed;
-        String hairColor = this.hairColorParsed;
-        String eyeColor = this.eyeColorParsed;
-        String birthYear = this.birthYearParsed;
 
         int [] imageResources = new int [] {R.drawable.tatooine, R.drawable.tatooinemos, R.drawable.dago, R.drawable.tatoo, R.drawable.stew, R.drawable.naboo, R.drawable.alderaan,
                                             R.drawable.corellia, R.drawable.kash};
 
-        DisplayPerson.tvName.setText(name);
-        DisplayPerson.tvHeight.setText(height);
-        DisplayPerson.tvWeight.setText(weight);
-        DisplayPerson.tvHomeWorld.setText(homeWorld);
-        DisplayPerson.tvNameTitle.setText(name);
-        DisplayPerson.tvHairColor.setText(hairColor);
-        DisplayPerson.tvEyeColor.setText(eyeColor);
-        DisplayPerson.tvBirthYear.setText(birthYear);
+        DisplayPerson.tvName.setText(this.nameParsed);
+        DisplayPerson.tvHeight.setText(this.heightParsed);
+        DisplayPerson.tvWeight.setText(this.weightParsed);
+        DisplayPerson.tvHomeWorld.setText(this.homeWorldParsed);
+        DisplayPerson.tvNameTitle.setText(this.nameParsed);
+        DisplayPerson.tvHairColor.setText(this.hairColorParsed);
+        DisplayPerson.tvEyeColor.setText(this.eyeColorParsed);
+        DisplayPerson.tvBirthYear.setText(this.birthYearParsed);
 
-        if(name.equals("Luke Skywalker")){
+        if(this.genderParsed.equals("Male")){
             DisplayPerson.rbGenderMale.toggle();
+        }
+        else if(this.genderParsed.equals("Female")){
+            DisplayPerson.rbGenderFemale.toggle();
+        }
+        else {
+            DisplayPerson.rbGenderNa.toggle();
+        }
+
+        if(this.nameParsed.equals("Luke Skywalker")){
             DisplayPerson.ivTopImage.setImageResource(imageResources[0]);
         }
-        else if (name.equals("C-3PO")){
-            DisplayPerson.rbGenderNa.toggle();
+        else if (this.nameParsed.equals("C-3PO")){
             DisplayPerson.ivTopImage.setImageResource(imageResources[1]);
         }
-        else if(name.equals("Yoda")) {
-            DisplayPerson.rbGenderMale.toggle();
+        else if(this.nameParsed.equals("Yoda")) {
             DisplayPerson.ivTopImage.setImageResource(imageResources[2]);
         }
-        else if(name.equals("Anakin Skywalker")){
-            DisplayPerson.rbGenderMale.toggle();
+        else if(this.nameParsed.equals("Anakin Skywalker")){
             DisplayPerson.ivTopImage.setImageResource(imageResources[3]);
         }
-        else if (name.equals("Obi-Wan Kenobi")){
-            DisplayPerson.rbGenderMale.toggle();
+        else if (this.nameParsed.equals("Obi-Wan Kenobi")){
             DisplayPerson.ivTopImage.setImageResource(imageResources[4]);
         }
-        else if(name.equals("Palpatine")) {
-            DisplayPerson.rbGenderMale.toggle();
+        else if(this.nameParsed.equals("Palpatine")) {
             DisplayPerson.ivTopImage.setImageResource(imageResources[5]);
         }
-        else if(name.equals("Leia Organa")){
-            DisplayPerson.rbGenderFemale.toggle();
+        else if(this.nameParsed.equals("Leia Organa")){
             DisplayPerson.ivTopImage.setImageResource(imageResources[6]);
         }
-        else if (name.equals("Han Solo")){
-            DisplayPerson.rbGenderMale.toggle();
+        else if (this.nameParsed.equals("Han Solo")){
             DisplayPerson.ivTopImage.setImageResource(imageResources[7]);
         }
-        else if(name.equals("Chewbacca")) {
-            DisplayPerson.rbGenderMale.toggle();
+        else if(this.nameParsed.equals("Chewbacca")) {
             DisplayPerson.ivTopImage.setImageResource(imageResources[8]);
         }
     }
